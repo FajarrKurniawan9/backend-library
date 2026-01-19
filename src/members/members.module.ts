c08eaa16308abfee50aa00ac20fc6be6e138b605
Muhammad Fajar Kurniawan <fk357421@gmail.com>
2026-01-19 09:48:17 +0700

    feat: add PrismaModule import to Books and Members modules

--- src/members/members.module.ts
+++ src/members/members.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MembersController } from './members.controller';
 import { MembersService } from './members.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [MembersController],
   providers: [MembersService],
 })
