d571e103e6062b5329b969075410a9657572f8ef
Muhammad Fajar Kurniawan <fk357421@gmail.com>
2026-01-19 10:33:59 +0700

    fix: update PrismaClient import path to use '@prisma/client'

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,7 +1,11 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '../generated/prisma';
+import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super();
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
